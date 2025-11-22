2950519997e42ddc1d1c8ccdb2fe8d2800a642a4
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-22 20:38:22 +0500

    Created component SectionHeader

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,7 +4,13 @@ import React from "react";
 const Featured = () => {
   const featuredBlogs = blogs.filter((blog) => blog.featured);
 
-  return <div></div>;
+  return (
+    <section className="text-gray-300 body-font bg-black/90">
+      <div className="container px-5 py-20 mx-auto">
+        <div className="items-center text-center pt-10 pb-20"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SectionHeader = ({
+  heading,
+  tagline,
+}: {
+  heading: string;
+  tagline: string;
+}) => {
+  return (
+    <>
+      <h1 className="title-font text-white text-6xl sm:text-7xl font-bold">
+        {heading}
+      </h1>
+      {/* <p className="text-primary mt-2">{tagline}</p> */}
+      <p className="text-primary mt-2">{tagline}</p>
+    </>
+  );
+};
+
+export default SectionHeader;
